99205a4b5a48cb7012044de52f55ec4ba33bfd3a
Claudio E. de Oliveira <claudio.oliveira@sensedia.com>
2020-03-29 17:50:40 -0300

    fix kafka producer

--- src/main/kotlin/kafka/study/advertisement/gateway/AdvertisementDispatcherVerticle.kt
+++ src/main/kotlin/kafka/study/advertisement/gateway/AdvertisementDispatcherVerticle.kt
@@ -3,13 +3,13 @@ package kafka.study.advertisement.gateway
 import io.cloudevents.kafka.CloudEventsKafkaProducer
 import io.cloudevents.v1.AttributesImpl
 import io.cloudevents.v1.CloudEventBuilder
-import io.cloudevents.v1.CloudEventImpl
 import io.cloudevents.v1.kafka.Marshallers
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.json.Json
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.ByteArraySerializer
+import org.apache.kafka.common.serialization.StringSerializer
 import java.net.URI
 import java.util.*
 
@@ -20,9 +20,9 @@ class AdvertisementDispatcherVerticle : AbstractVerticle() {
 
     val props = Properties()
     props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = ByteArraySerializer::class.java
-    props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = System.getenv("KAFKA_HOST")
+    props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+    props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = if (System.getenv("KAFKA_HOST") != null) System.getenv("KAFKA_HOST") else "0.0.0.0:9092"
     props[ProducerConfig.CLIENT_ID_CONFIG] = "advertisement-gateway"
-
 
     this.vertx.eventBus().consumer<String>("advertisement-request") {
       val opportunity = Json.decodeValue(it.body(), Opportunity::class.java)
--- src/main/kotlin/kafka/study/advertisement/gateway/AdvertisementReceiverVerticle.kt
+++ src/main/kotlin/kafka/study/advertisement/gateway/AdvertisementReceiverVerticle.kt
@@ -1,10 +1,12 @@
 package kafka.study.advertisement.gateway
 
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.google.common.hash.Hashing
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.core.json.Json
+import io.vertx.core.json.jackson.DatabindCodec
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.handler.BodyHandler
@@ -13,22 +15,23 @@ import java.nio.charset.StandardCharsets
 class AdvertisementReceiverVerticle : AbstractVerticle() {
 
   override fun start() {
+    DatabindCodec.mapper().registerModule(KotlinModule())
     var router = Router.router(vertx)
     router.route().handler(BodyHandler.create())
-    router.post("/advertisement").handler { receiveAdvRequest(it,this.vertx) }
+    router.post("/advertisements").handler { receiveAdvRequest(it,this.vertx) }
     vertx.createHttpServer().requestHandler(router).listen(9999)
   }
 }
 
 fun receiveAdvRequest(routingContext: RoutingContext,vertx: Vertx) {
-  val customerKey = routingContext.request().getParam("X-Customer-Key")
+  val customerKey = routingContext.request().getHeader("X-Customer-Key")
   if (customerKey == null) {
     routingContext.response().error(422)
   } else {
     val advReq = Json.decodeValue(routingContext.getBodyAsJson().toBuffer(), AdvertisementRequest::class.java)
     val opportunityId = Hashing.sha256().hashString(Json.encode(advReq), StandardCharsets.UTF_8).toString();
     val opportunity = Opportunity(id = opportunityId, customerKey = customerKey, advReq = advReq)
-    vertx.eventBus().send("advertisement-request",opportunity)
+    vertx.eventBus().send("advertisement-request",Json.encode(opportunity))
     routingContext.response().jsonCreated(opportunity)
   }
 }
