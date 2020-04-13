a5b59a01a57c2aad4872b4e55e08493abff3a92c
Luram Archanjo <luram.archanjo@sensedia.com>
2020-04-13 20:02:57 -0300

    Fixing build

--- src/main/kotlin/kafka/study/advertisement/gateway/AdvertisementGateway.kt
+++ src/main/kotlin/kafka/study/advertisement/gateway/AdvertisementGateway.kt
@@ -2,12 +2,23 @@ package kafka.study.advertisement.gateway
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
+import io.vertx.core.Vertx
 
 class AdvertisementGateway : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>) {
     vertx.deployVerticle(AdvertisementReceiverVerticle())
     vertx.deployVerticle(AdvertisementDispatcherVerticle())
+  }
+
+  companion object {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+      val vertx = Vertx.vertx()
+      vertx.deployVerticle(AdvertisementGateway::class.java.name)
+    }
+
   }
 
 }
